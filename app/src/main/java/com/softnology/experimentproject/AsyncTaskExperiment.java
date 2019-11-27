6b68f49b4a92c85a9520944a31a4bfa0c83b883b
Ali Adil <aliadilg@gmail.com>
2019-11-27 13:53:00 +0500

    Toast implementation with different Gravity Set

--- app/src/main/java/com/softnology/experimentproject/AsyncTaskExperiment.java
+++ app/src/main/java/com/softnology/experimentproject/AsyncTaskExperiment.java
@@ -1,10 +1,8 @@
 package com.softnology.experimentproject;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -15,22 +13,20 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 
 public class AsyncTaskExperiment extends AppCompatActivity {
 
     private static final String TEXT_STATE = "currentText";
+    Integer count = 1;
     private TextView mTextView = null;
     private TextView textViewProgress = null;
     private ProgressBar progressBar;
-    Integer count =1;
-    private Toast toast, toast2,toast3,toast4,toast5;
+    private Toast toast;
     private Context context;
-    private  int duration;
+    private int duration;
     private ArrayList<CharSequence> charSequence;
 
     @Override
@@ -39,97 +35,109 @@ public class AsyncTaskExperiment extends AppCompatActivity {
         setContentView(R.layout.activity_async_task);
         mTextView = findViewById(R.id.textView1);
         textViewProgress = findViewById(R.id.TextView_Progress);
-        progressBar = (ProgressBar) findViewById(R.id.Progressbar);
-        Button showToast =  findViewById(R.id.btn_showToast);
+        progressBar = findViewById(R.id.Progressbar);
+        Button showToast = findViewById(R.id.btn_showToast);
         progressBar.setProgress(0);
+
+        //Application context
         context = getApplicationContext();
+
+        // Char Sequence Array For Toast Text
         charSequence = new ArrayList<>();
-        for (int i = 0 ; i < 5 ; i ++){
-            charSequence.add("This is my Toast Test #"+(i+1));
+        for (int i = 0; i < 5; i++) {
+            charSequence.add("This is my Toast Test #" + (i + 1));
         }
 
+        // Duration of toast
         duration = Toast.LENGTH_SHORT;
 
+        // Show Toast Click Listener
         showToast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                toast = Toast.makeText(context,charSequence.get(0),duration);
-                toast.setGravity(Gravity.TOP|Gravity.START,0,0);
+                toast = Toast.makeText(context, charSequence.get(0), duration);
+                toast.setGravity(Gravity.TOP | Gravity.START, 0, 0);
                 toast.show();
 
-                toast2 = Toast.makeText(context,charSequence.get(1),duration) ;
-                toast2.setGravity(Gravity.BOTTOM|Gravity.END,0,0);
-                toast2.show();
+                toast = Toast.makeText(context, charSequence.get(1), duration);
+                toast.setGravity(Gravity.BOTTOM | Gravity.END, 0, 0);
+                toast.show();
 
-                toast3 = Toast.makeText(context,charSequence.get(2),duration) ;
-                toast3.setGravity(Gravity.CENTER|Gravity.START,0,600);
-                toast3.show();
+                toast = Toast.makeText(context, charSequence.get(2), duration);
+                toast.setGravity(Gravity.CENTER | Gravity.START, 0, 600);
+                toast.show();
 
-                toast4 = Toast.makeText(context,charSequence.get(3),duration) ;
-                toast4.setGravity(Gravity.CENTER|Gravity.START,100,-100);
-                toast4.show();
+                toast = Toast.makeText(context, charSequence.get(3), duration);
+                toast.setGravity(Gravity.CENTER | Gravity.START, 100, -100);
+                toast.show();
 
-                toast4 = Toast.makeText(context,charSequence.get(4),duration) ;
-                toast4.setGravity(Gravity.TOP|Gravity.START,500,300);
-                toast4.show();
+                toast = Toast.makeText(context, charSequence.get(4), duration);
+                toast.setGravity(Gravity.TOP | Gravity.START, 500, 300);
+                toast.show();
 
 
             }
         });
 
 
-
-
+        // getting text from saveInstanceState
         if (savedInstanceState != null) {
             mTextView.setText(savedInstanceState.getString(TEXT_STATE));
         }
     }
 
+    // Saving putting text in bundle so that i can get my text when screen is rotated
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(TEXT_STATE, mTextView.getText().toString());
     }
 
+    // Method is attached with start task button to start async task
     public void startTask(View view) {
-
         mTextView.setText(R.string.napping);
-        count =1;
+        count = 1;
         Random r = new Random();
         int multiple = 1000;
         int number = r.nextInt(11);
         int miliSec = number * multiple;
-        int milToSec = miliSec/1000;
+        // converting milliseconds to seconds
+        int milToSec = miliSec / 1000;
+        // set progress max to total seconds
         progressBar.setMax(milToSec);
-        new SimpleAsyncTask(mTextView,textViewProgress,progressBar,AsyncTaskExperiment.this).execute(miliSec);
+        // executing async task
+        new SimpleAsyncTask(mTextView, textViewProgress, progressBar, AsyncTaskExperiment.this).execute(miliSec);
 
 
     }
 
+    // Simple Async Task with progress bar
+    // AsyncTask is an Abstract Helper class
     public static class SimpleAsyncTask extends AsyncTask<Integer, Integer, String> {
+        float valueCalcu = 0;
         private WeakReference<TextView> mTextView;
         private WeakReference<TextView> textViewProgress;
-        private SoftReference<Context> context;
         private WeakReference<ProgressBar> _Bar;
-        private int milToSec  = 0;
-        float valueCalcu = 0;
-        SimpleAsyncTask(TextView tv,TextView tv2,ProgressBar _progressBar ,Context _context) {
+        private int milToSec = 0;
+
+        SimpleAsyncTask(TextView tv, TextView tv2, ProgressBar _progressBar, Context _context) {
             mTextView = new WeakReference<>(tv);
             textViewProgress = new WeakReference<>(tv2);
-            context = new SoftReference<>(_context);
             _Bar = new WeakReference<>(_progressBar);
         }
 
 
         @Override
-        protected void onPreExecute(){
+        protected void onPreExecute() {
             textViewProgress.get().setText(R.string.task_starting);
         }
 
         @Override
         protected String doInBackground(Integer... integers) {
-            milToSec = integers[0]/1000;
-            for (int count = 1 ; count <= milToSec ; count++) {
+            // milliseconds to seconds
+            milToSec = integers[0] / 1000;
+
+            for (int count = 1; count <= milToSec; count++) {
                 try {
                     Thread.sleep(integers[0]);
 
@@ -138,12 +146,9 @@ public class AsyncTaskExperiment extends AppCompatActivity {
                     e.printStackTrace();
 
                 }
+                //calling progress update
                 publishProgress(count);
             }
-
-
-
-
 
 
             // Return a String result
@@ -152,18 +157,18 @@ public class AsyncTaskExperiment extends AppCompatActivity {
 
         @Override
         protected void onProgressUpdate(Integer... values) {
-           /* super.onProgressUpdate(values);
-            textViewProgress.get().setText(Arrays.toString(values));*/
+
             if (this._Bar != null) {
-                valueCalcu = ((float)values[0]/(float)milToSec)*100;
-                textViewProgress.get().setText(valueCalcu +"%");
+                // Converting
+                valueCalcu = ((float) values[0] / (float) milToSec) * 100;
+                textViewProgress.get().setText(valueCalcu + "%");
                 _Bar.get().setProgress(values[0]);
             }
         }
 
         protected void onPostExecute(String result) {
             mTextView.get().setText(result);
-            textViewProgress.get().setText("Task "+valueCalcu +"%"+" Completed");
+            textViewProgress.get().setText("Task " + valueCalcu + "%" + " Completed");
         }
 
 
