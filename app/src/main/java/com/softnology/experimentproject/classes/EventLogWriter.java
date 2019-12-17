9b44ba0bd37d56febd26b6f2f61c4e6d92ea9cf9
Ali Adil <aliadilg@gmail.com>
2019-12-17 20:53:54 +0500

    Dependency

---
+++ app/src/main/java/com/softnology/experimentproject/classes/EventLogWriter.java
@@ -0,0 +1,7 @@
+package com.softnology.experimentproject.classes;
+/*  Dependency inversion principle is a software design principle which provides us the guidelines to write loosely coupled classes.
+    According to the definition of Dependency inversion principle:
+    # High-level modules should not depend on low-level modules. Both should depend on abstractions.
+    # Abstractions should not depend upon details. Details should depend upon abstractions.*/
+public class EventLogWriter {
+}
